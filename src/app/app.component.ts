ff7093af808ef1eb67e748fa5bd2f2c8e0b220bd
Pratimashendge <shendgepratima05@gmail.com>
2025-01-05 18:54:00 +0530

    done changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'catlog';
-
+  selectedOption:string='product';
   productsData=[
     {
       pname:"Samsung",
@@ -20,6 +20,7 @@ export class AppComponent {
       pcatg:"product"
     }
   ]
+
  addproduct(){
   const newproducts={
     pname:`Iphone`,
@@ -30,12 +31,13 @@ export class AppComponent {
    
 
   this.productsData.push(newproducts);
-
-  const newcategory={
+ }
+ addcatlog(){
+  const newcatlog={
     pname:`Samsung`,
     pdescription:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero enim ullam voluptates qui ea. Beatae, quos! Fuga fugiat voluptatibus, totam odit voluptatum dicta, temporibus earum unde facilis, fugit quibusdam atque.`,
     pcatg:`catlog`
   }
-  this.productsData.push(newcategory);
+  this.productsData.push(newcatlog);
  }
 }
